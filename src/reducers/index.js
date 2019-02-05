4b6dddf2806fa94d068325138f76677e460303e4
Wilfried LLOZA <wilfried.lloza@gmail.com>
2019-02-05 16:06:08 +0100

    add actions and reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,19 @@
+const initialState = {
+  angular: 0,
+  react: 0,
+  vuejs: 0
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case "VOTE_ANGULAR":
+      return { ...state, angular: state.angular + 1 };
+    case "VOTE_REACT":
+      return { ...state, angular: state.react + 1 };
+    case "VOTE_VUEJS":
+      return { ...state, angular: state.vuejs + 1 };
+
+    default:
+      return state;
+  }
+};
